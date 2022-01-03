e7094bcd9d07ed0f6e916de3352049db803d6892
Himanshu Raj <himanshurajkush@gmail.com>
2022-01-03 13:11:38 +0530

    Makefile: product copy dtb

--- twrp_lmi.mk
+++ twrp_lmi.mk
@@ -20,6 +20,9 @@ $(call inherit-product, device/xiaomi/lmi/device.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
 
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
+    $(LOCAL_PATH)/prebuilt/dtb:dtb.img
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lmi
 PRODUCT_NAME := twrp_lmi
