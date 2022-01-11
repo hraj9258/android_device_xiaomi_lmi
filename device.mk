02e42404a77912921e294e31b99f32c6e21477a4
Himanshu Raj <himanshurajkush@gmail.com>
2022-01-11 21:01:42 +0530

    Device.mk: Manully set BUILD_FINGERPRINT_FROM_FILE

--- device.mk
+++ device.mk
@@ -38,6 +38,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_HOST_PACKAGES += \
     libandroidicu
 
+BUILD_FINGERPRINT_FROM_FILE := Redmi/lmi/lmi:11/RKQ1.200826.002/V12.5.5.0.RJKMIXM:user/release-keys
+
 # HACK: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.security_patch=2099-12-31
